5d5ab4fc87c9d492740f38f4b948fbbc30ed9af6
WannaBSteve <103111413+WannaBSteve@users.noreply.github.com>
2022-10-26 16:01:10 +0800

    Add files via upload

--- 飞机大战/飞机大战/源.cpp
+++ 飞机大战/飞机大战/源.cpp
@@ -4,6 +4,7 @@
 #include<conio.h>
 #include<time.h>
 #include"tools.h"
+#pragma comment(lib,"Winmm.lib")
 
 #define BULLET_NUM 15
 #define ENEMY_NUM 10
@@ -24,13 +25,17 @@ bool Timer(int ms, int id) {
 struct Plane {
 	int x, y;
 	int HP;//生命值
-	int sheild;
-	bool haveshield;
+	int sheild;//护盾
+	bool haveshield;//飞机是否还有护盾
+	int rank;//用rank军衔来表示得分，当军衔为3的时候就通关了
+	double score;//score为得分，每击落一个敌人得0.2分，得分大于军衔的时候说明该升级军衔了
 	bool isDie;//飞机是否死亡
 	bool undefeat;//飞机是否是无敌状态
 	int crashtime;//最近一次发生撞击的时间
 	int frame;//帧
 	int hitframe;//给护盾做特殊动画的帧
+	int rankframe;//晋升军衔的动画帧
+	
 
 	void setUndefeat();
 	void delUndefeat();
@@ -46,14 +51,17 @@ IMAGE img_gamerhit[2];//护盾耗尽时，玩家飞机被撞击之后的图片
 IMAGE img_sheildhit[5];//还有护盾时，玩家飞机被撞击之后的图片
 IMAGE img_bk;		//背景图，也是贴图的主界面
 IMAGE img_bullet[2];
+IMAGE img_raser[5];
 IMAGE img_enemy[2];
 IMAGE img_healthbar[7];
 IMAGE img_hp;
 IMAGE img_sheildbar[5];
+IMAGE img_rank[6][12];
 
 
 
 
+void drawRank(Plane* pthis, int Rankframe);
 void drawHpbar(Plane* plane);
 void drawsheildbar(Plane* plane);
 //飞机初始化
@@ -67,6 +75,8 @@ void init_Plane(Plane* pthis, int x, int y) {
 	pthis->sheild = 4;
 	pthis->haveshield = true;
 	pthis->undefeat = false;	//最开始不是无敌状态，只有扣了血的一段时间内或者捡到道具才进入
+	pthis->rank = 0;
+	pthis->score = -1;//!!!!!!!注意这里要从-1开始，才能让score大于军衔的时候晋升军衔
 }
 //绘制飞机
 void draw_Plane(Plane* pthis) {
@@ -81,10 +91,26 @@ void draw_Plane(Plane* pthis) {
 		}
 		else drawAlpha(img_sheildhit + pthis->hitframe, pthis->x, pthis->y, 102, 126, 0, 0, 1);
 	}
+	int Rankframe = pthis->rankframe;
+	drawRank(pthis,Rankframe);
 	drawHpbar(pthis);
 	drawsheildbar(pthis);
+	pthis->rankframe = (pthis->rankframe + 1) % 12;
 	pthis->hitframe = (pthis->hitframe + 1) % 5;
 	pthis->frame = (pthis->frame + 1) % 2;
+
+	LOGFONT f;
+	gettextstyle(&f);						// 获取当前字体设置
+	f.lfHeight = 48;						// 设置字体高度为 48
+	_tcscpy(f.lfFaceName, _T("黑体"));		// 设置字体为“黑体”(高版本 VC 推荐使用 _tcscpy_s 函数)
+	f.lfQuality = ANTIALIASED_QUALITY;		// 设置输出效果为抗锯齿  
+	TCHAR fontt[] = "黑体";
+	TCHAR score[] = "Score ";
+	char s[2];
+	settextstyle(18, 10, fontt);
+	outtextxy(365, 100, score);
+	sprintf(s, "%d", pthis->rank);
+	outtextxy(420, 100, s);
 }
 //移动飞机
 void move_Plane(Plane* pthis) {
@@ -107,7 +133,8 @@ void move_Plane(Plane* pthis) {
 	}
 	//判断是否撞到敌机而死
 	for (int i = 0; i < ENEMY_NUM; ++i) {
-		
+		if (pthis->score > 5) { pthis->isDie = true; break; }
+		if (pthis->score >= pthis->rank)pthis->rank++;
 		if (pthis->HP == 0) {
 			pthis->isDie = true;		
 			break;
@@ -176,7 +203,7 @@ void createEnemy() {
 	}
 }
 //移动敌机
-void moveEnemy() {
+void moveEnemy(Plane*pthis) {
 	int speed = 3;
 	for (int i = 0; i < ENEMY_NUM; ++i) {
 		if (!enemy[i].isDie) {
@@ -189,6 +216,8 @@ void moveEnemy() {
 					if (bullet[j].x >= enemy[i].x && bullet[j].x <= enemy[i].x + img_enemy->getwidth() && bullet[j].y >= enemy[i].y && bullet[j].y <= enemy[i].y + img_enemy->getheight()) {
 						enemy[i].isDie = true;
 						bullet[j].isDie = true;
+						//还要加得分用来升级rank
+						pthis->score += 0.2;
 					}
 				}
 			}
@@ -211,7 +240,7 @@ void moveBullet() {
 void loadResource() {
 	//背景
 	
-	loadimage(&img_bk, "background.png", 480, 850);
+	loadimage(&img_bk, "Nebula.png", 480, 850); 
 	loadimage(img_gamer + 0, "me1.png");
 	loadimage(img_gamer + 1, "me2.png");
 	loadimage(img_gamerhit + 0, "me1_hit.png");
@@ -224,6 +253,11 @@ void loadResource() {
 	//加载子弹
 	loadimage(img_bullet + 0, "bullet1.png");
 	loadimage(img_bullet + 1, "bullet2.png");
+	loadimage(img_raser+0, "raserbullet0.png");
+	loadimage(img_raser + 1, "raserbullet1.png");
+	loadimage(img_raser + 2, "raserbullet2.png");
+	loadimage(img_raser + 3, "raserbullet3.png");
+	loadimage(img_raser + 4, "raserbullet4.png");
 	//敌机
 	loadimage(img_enemy + 0, "enemy1.png");
 	loadimage(img_enemy + 1, "enemy2.png");
@@ -237,6 +271,84 @@ void loadResource() {
 	loadimage(img_sheildbar + 2, "sheildbar-2.png");
 	loadimage(img_sheildbar + 3, "sheildbar-3.png");
 	loadimage(img_sheildbar + 4, "sheildbar-4.png");
+
+	loadimage(img_rank[0] + 0, "1@64px.png");
+	loadimage(img_rank[0] + 1, "1@64px.png");
+	loadimage(img_rank[0] + 2, "2@64px.png");
+	loadimage(img_rank[0] + 3, "2@64px.png");
+	loadimage(img_rank[0] + 4, "3@64px.png");
+	loadimage(img_rank[0] + 5, "3@64px.png");
+	loadimage(img_rank[0] + 6, "4@64px.png");
+	loadimage(img_rank[0] + 7, "4@64px.png");
+	loadimage(img_rank[0] + 8, "5@64px.png");
+	loadimage(img_rank[0] + 9, "5@64px.png");
+	loadimage(img_rank[0] + 10, "6@64px.png");
+	loadimage(img_rank[0] + 11, "6@64px.png");
+
+	loadimage(img_rank[1] + 0, "1.1.png");
+	loadimage(img_rank[1] + 1, "1.1.png");
+	loadimage(img_rank[1] + 2, "1.2.png");
+	loadimage(img_rank[1] + 3, "1.2.png");
+	loadimage(img_rank[1] + 4, "1.3.png");
+	loadimage(img_rank[1] + 5, "1.3.png");
+	loadimage(img_rank[1] + 6, "1.4.png");
+	loadimage(img_rank[1] + 7, "1.4.png");
+	loadimage(img_rank[1] + 8, "1.5.png");
+	loadimage(img_rank[1] + 9, "1.5.png");
+	loadimage(img_rank[1] + 10, "1.6.png");
+	loadimage(img_rank[1] + 11, "1.6.png");
+
+	loadimage(img_rank[2] + 0, "2.1.png");
+	loadimage(img_rank[2] + 1, "2.1.png");
+	loadimage(img_rank[2] + 2, "2.2.png");
+	loadimage(img_rank[2] + 3, "2.2.png");
+	loadimage(img_rank[2] + 4, "2.3.png");
+	loadimage(img_rank[2] + 5, "2.3.png");
+	loadimage(img_rank[2] + 6, "2.4.png");
+	loadimage(img_rank[2] + 7, "2.4.png");
+	loadimage(img_rank[2] + 8, "2.5.png");
+	loadimage(img_rank[2] + 9, "2.5.png");
+	loadimage(img_rank[2] + 10, "2.6.png");
+	loadimage(img_rank[2] + 11, "2.6.png"); 
+
+	loadimage(img_rank[3] + 0, "3.1.png");
+	loadimage(img_rank[3] + 1, "3.1.png");
+	loadimage(img_rank[3] + 2, "3.2.png");
+	loadimage(img_rank[3] + 3, "3.2.png");
+	loadimage(img_rank[3] + 4, "3.3.png");
+	loadimage(img_rank[3] + 5, "3.3.png");
+	loadimage(img_rank[3] + 6, "3.4.png");
+	loadimage(img_rank[3] + 7, "3.4.png");
+	loadimage(img_rank[3] + 8, "3.5.png");
+	loadimage(img_rank[3] + 9, "3.5.png");
+	loadimage(img_rank[3] + 10, "3.6.png");
+	loadimage(img_rank[3] + 11, "3.6.png");
+
+	loadimage(img_rank[4] + 0, "4.1.png");
+	loadimage(img_rank[4] + 1, "4.1.png");
+	loadimage(img_rank[4] + 2, "4.2.png");
+	loadimage(img_rank[4] + 3, "4.2.png");
+	loadimage(img_rank[4] + 4, "4.3.png");
+	loadimage(img_rank[4] + 5, "4.3.png");
+	loadimage(img_rank[4] + 6, "4.4.png");
+	loadimage(img_rank[4] + 7, "4.4.png");
+	loadimage(img_rank[4] + 8, "4.5.png");
+	loadimage(img_rank[4] + 9, "4.5.png");
+	loadimage(img_rank[4] + 10, "4.6.png");
+	loadimage(img_rank[4] + 11, "4.6.png");
+
+	loadimage(img_rank[5] + 0, "5.1.png");
+	loadimage(img_rank[5] + 1, "5.1.png");
+	loadimage(img_rank[5] + 2, "5.2.png");
+	loadimage(img_rank[5] + 3, "5.2.png");
+	loadimage(img_rank[5] + 4, "5.3.png");
+	loadimage(img_rank[5] + 5, "5.3.png");
+	loadimage(img_rank[5] + 6, "5.4.png");
+	loadimage(img_rank[5] + 7, "5.4.png");
+	loadimage(img_rank[5] + 8, "5.5.png");
+	loadimage(img_rank[5] + 9, "5.5.png");
+	loadimage(img_rank[5] + 10, "5.6.png");
+	loadimage(img_rank[5] + 11, "5.6.png");
 
 	//加载help等button
 	
@@ -267,6 +379,11 @@ void init() {
 		enemy[i].isDie = true;
 	}
 }
+void drawRank(Plane* plane,int Rankframe) {
+	int i = plane->rank-1;
+	if(i>=0&&i<=4)
+	drawAlpha(img_rank[i] + Rankframe, 385, 10, 128, 128, 0, 0, 1);
+}
 void drawHpbar(Plane* plane) {
 	int i = 6 - plane->HP;
 	drawAlpha(img_healthbar+i, 0, 5, 150, 34, 0, 0, 1);
@@ -285,7 +402,9 @@ void draw() {
 	//子弹
 	for (int i = 0; i < BULLET_NUM; ++i) {
 		if (!bullet[i].isDie) {
-			drawAlpha(img_bullet + 0, bullet[i].x, bullet[i].y, 5, 11, 0, 0, 1);
+			int j = gamer.score;
+			drawAlpha(img_raser+j, bullet[i].x, bullet[i].y, 80, 80, 0, 0, 1);
+			drawAlpha(img_raser+j, gamer.x, bullet[i].y, 80, 80, 0, 0, 1);
 		}
 	}
 	//敌机
@@ -392,24 +511,25 @@ void drawAlpha(IMAGE* dstimg, int x, int y, IMAGE* srcimg)
 		}
 	}
 }
-int main() {
+void Play() {
 	//创建窗口
 	initgraph(480, 850);
-L1: loadimage(&img_bk, "background.png",480,850);
+L1: loadimage(&img_bk, "Nebula.png", 480, 850);
 	loadimage(img_healthbar + 0, "healthbar.png");
-	loadimage(img_healthbar+1, "healthbar-1.png");
+	loadimage(img_healthbar + 1, "healthbar-1.png");
 	loadimage(img_healthbar + 2, "healthbar-2.png");
 	loadimage(img_healthbar + 3, "healthbar-3.png");
 	loadimage(img_healthbar + 4, "healthbar-4.png");
 	loadimage(img_healthbar + 5, "healthbar-5.png");
 	loadimage(img_healthbar + 6, "healthbar-6.png");
-	
+
 	loadimage(img_sheildbar + 0, "sheildbar.png");
 	loadimage(img_sheildbar + 1, "sheildbar-1.png");
 	loadimage(img_sheildbar + 2, "sheildbar-2.png");
 	loadimage(img_sheildbar + 3, "sheildbar-3.png");
 	loadimage(img_sheildbar + 4, "sheildbar-4.png");
 	putimage(0, 0, &img_bk);
+
 
 	TCHAR s[50] = "1.开始游戏";
 	TCHAR s1[50] = "2.游戏帮助";
@@ -419,7 +539,9 @@ L1: loadimage(&img_bk, "background.png",480,850);
 	button(155, 510, 170, 50, s2);
 	MOUSEMSG m;
 	while (true) {
-
+		//播放音乐
+		mciSendString("open cornfield_chase.mp3 alias mymusic", NULL, 0, NULL);
+		mciSendString("play mymusic", NULL, 0, NULL);
 		m = GetMouseMsg();
 		if (m.uMsg == WM_LBUTTONDOWN) {
 			if (m.x >= 155 && m.x <= 325 && m.y >= 210 && m.y <= 260) {
@@ -430,25 +552,28 @@ L1: loadimage(&img_bk, "background.png",480,850);
 					draw();
 					move_Plane(&gamer);
 					if (gamer.isDie) {
-						return 0;
+						HWND wnd = GetHWnd();
+						if (MessageBox(wnd, "您是否想要重来一局？", "飞机已阵亡！", MB_YESNO) == 6)Play();
+						else
+							return;
 					}
 					moveBullet();
 					//隔一段时间产生一个敌机
 					if (Timer(200, 1)) {
 						createEnemy();
 					}
-					moveEnemy();
+					moveEnemy(&gamer);
 					int frameTime = clock() - startTime;
 					//一帧应该执行的时间大于当前帧执行的时间（提前完成了）
 					if (1000 / 60 - frameTime > 0) {
 						Sleep(1000 / 60 - frameTime);
 					}
-					
+
 					//双缓冲消除闪屏
 					drawAlpha(&flushimg, 0, 0, &img_bk);
 					drawAlpha(&img_bk, 0, 0, &flushimg);
 				}
-				
+
 				system("pause");
 			}
 			else if (m.x >= 155 && m.x <= 325 && m.y >= 360 && m.y <= 410) {
@@ -459,11 +584,15 @@ L1: loadimage(&img_bk, "background.png",480,850);
 				goto L1;
 			}
 			else if (m.x >= 155 && m.x <= 325 && m.y >= 510 && m.y <= 560) {
-				return 0;
+				return ;
 			}
 		}
 	}
 	system("pause");
+	return ;
+}
+int main() {
+	Play();
 	return 0;
 }
 
